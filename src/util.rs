8dc49c07e80cf97d2e4d24325a4df4420ad1e28c
bepzi <dev.ben01189998819991197253@gmail.com>
2017-04-05 11:27:19 -0500

    feat: add workaround method for Config::get_array()
    Version `0.4.1` of `config-rs` has a bug where the
    `Config::get_array()` method consumes `self` instead of taking `self`
    by reference like its other methods. The Rust compiler then complains
    that the config variable has been moved and disallows any future use
    of it. The bug has been fixed by the authors of `config-rs`, but at
    this time the fix has not been released on crates.io. Until then, our
    new function, `ref_get_array()`, will serve that purpose.

--- src/util.rs
+++ src/util.rs
@@ -1,9 +1,36 @@
-pub fn get_array() {}
+use config::{Config, Value};
+
+/// Gets an array from a config file using a key, returning `None` if
+/// the key wasn't present or couldn't be coerced into a `Vec<Value>`.
+///
+/// `Config::get_array()` in version `0.4.1` has a bug where it
+/// consumes `self` instead of taking `self` by reference. This
+/// function is a workaround for the time being. The bug has been
+/// fixed but will not be available until the release of version `0.5`
+/// of `config-rs`.
+pub fn ref_get_array(key: &str, config: &Config) -> Option<Vec<Value>> {
+    config.get(key).and_then(Value::into_array)
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn test_get_array() {
-        assert!(true)
+    // The real test is that this compiles successfully without giving
+    // a warning about the config file being moved.
+    fn test_ref_get_array() {
+        let mut c: Config = Config::new();
+        c.set("numbers", vec![1, 2, 3]).unwrap();
+        c.set("boolean", true).unwrap();
+
+        // Uncomment and the compiler should complain
+        // assert_eq!(c.get_array("numbers"),
+        //            Some(vec![Value::Integer(1), Value::Integer(2), Value::Integer(3)]));
+        // assert_eq!(c.get_array("boolean"), None); // Use of moved value: c
+
+        assert_eq!(ref_get_array("numbers", &c),
+                   Some(vec![Value::Integer(1), Value::Integer(2), Value::Integer(3)]));
+        assert_eq!(ref_get_array("boolean", &c), None);
     }
 }
