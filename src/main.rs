1295bd5f1fb981b90b755fcfabe4cd5a9cf8b5ac
ben01189998819991197253 <ben01189998819991997253@users.noreply.github.com>
2018-02-02 17:54:01 -0600

    Add basic config file retrieval
    Retrieves the main config file from
    "$XDG_CONFIG_HOME/contrail/config.toml" and the module config files from
    "$XDG_CONFIG_HOME/contrail/modules/" if there are any. Panics with an
    error message if either the CLI-specified folder path or the default one
    doesn't exist. In the future, it should not be an error for the default
    config file to not exist; we should just use the program defaults.
    
    Resolves: #24

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,32 @@ extern crate config;
 extern crate glob;
 
 use clap::{App, Arg};
-use config::{Config, File};
+use config::Config;
 use glob::glob;
+
+use std::path::PathBuf;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const APP_NAME: &str = "contrail";
+const HOME: &str = env!("HOME");
 
 fn main() {
+    const MAIN_CONFIG_NAME: &str = "config";
+
     let matches = App::new(APP_NAME)
         .version(VERSION)
         .about("Fast and configurable shell prompter")
-        .arg(Arg::with_name("exit_code")
-                 .short("e")
-                 .long("exit_code")
-                 .value_name("CODE")
-                 .help("Exit code of the last-executed command")
-                 .takes_value(true))
+        // .arg(Arg::with_name("exit_code")
+        //          .short("e")
+        //          .long("exit_code")
+        //          .value_name("CODE")
+        //          .help("Exit code of the last-executed command")
+        //          .takes_value(true))
         .arg(Arg::with_name("config")
                  .short("c")
                  .long("config")
-                 .value_name("FILE")
-                 .help("Location of configuration file")
+                 .value_name("PATH")
+                 .help("Location of a folder containing config files")
                  .takes_value(true))
         // .arg(Arg::with_name("shell")
         //          .long("shell")
@@ -31,19 +36,40 @@ fn main() {
         //          .takes_value(true)
         //          .possible_values(&["bash", "zsh", "fish", "powershell"]))
         .get_matches();
-    
+
     let mut settings = Config::default();
 
-    if let Some(f) = matches.value_of("config") {
-        // Merge in the default configs
-        // c.merge(File::new(f, FileFormat::Toml).required(false))
-        //     .expect("Failed to merge in config file!");
+    let config_folder: PathBuf = if let Some(path) = matches.value_of("config") {
+        PathBuf::from(path)
+    } else {
+        // User didn't specify a custom folder for config files
+        [HOME, ".config", APP_NAME].iter().collect()
+    };
+
+    // TODO: Don't panic if we're using the default folder location
+    if !config_folder.exists() {
+        panic!(
+            "config folder path \"{}\" does not exist",
+            config_folder.to_str().unwrap()
+        );
     }
 
-    // Merge in the module config files
-    settings.merge(glob("conf/*")
-                   .unwrap()
-                   .map(|path| File::from(path.unwrap()))
-                   .collect::<Vec<_>>())
+    let config_folder = config_folder.to_str().unwrap();
+
+    settings
+        .merge(config::File::with_name(&format!(
+            "{}/{}",
+            config_folder, MAIN_CONFIG_NAME
+        )))
+        .unwrap();
+
+    // Merge all the module config files
+    settings
+        .merge(
+            glob(&format!("{}/modules/*", config_folder))
+                .unwrap()
+                .map(|path| config::File::from(path.unwrap()))
+                .collect::<Vec<_>>(),
+        )
         .unwrap();
 }
