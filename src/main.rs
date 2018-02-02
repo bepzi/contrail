f854f4f7668f4d79a29de0e8bf3674aaaeca6612
ben01189998819991197253 <ben01189998819991997253@users.noreply.github.com>
2018-02-02 13:21:08 -0600

    Temporarily disable config location option
    Our current use of clap-rs is fine. Adds the glob crate, as we will want
    to use globbing to retrieve the module-specific config files from
    "$XDG_CONFIG_HOME/contrail/modules/".
    
    Resolves: #23

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate clap;
 extern crate config;
+extern crate glob;
 
 use clap::{App, Arg};
-use config::{Config, File, FileFormat};
+use config::{Config, File};
+use glob::glob;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const APP_NAME: &str = "contrail";
@@ -23,17 +25,25 @@ fn main() {
                  .value_name("FILE")
                  .help("Location of configuration file")
                  .takes_value(true))
-        .arg(Arg::with_name("shell")
-                 .long("shell")
-                 .value_name("SHELL")
-                 .takes_value(true)
-                 .possible_values(&["bash", "zsh", "fish", "powershell"]))
+        // .arg(Arg::with_name("shell")
+        //          .long("shell")
+        //          .value_name("SHELL")
+        //          .takes_value(true)
+        //          .possible_values(&["bash", "zsh", "fish", "powershell"]))
         .get_matches();
     
-    let mut c = Config::new();
+    let mut settings = Config::default();
 
     if let Some(f) = matches.value_of("config") {
-        c.merge(File::new(f, FileFormat::Toml).required(false))
-            .expect("Failed to merge in config file!");
+        // Merge in the default configs
+        // c.merge(File::new(f, FileFormat::Toml).required(false))
+        //     .expect("Failed to merge in config file!");
     }
+
+    // Merge in the module config files
+    settings.merge(glob("conf/*")
+                   .unwrap()
+                   .map(|path| File::from(path.unwrap()))
+                   .collect::<Vec<_>>())
+        .unwrap();
 }
