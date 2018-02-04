eb4809ddf5201be289e1b580689539db9ec89a14
ben01189998819991197253 <ben01189998819991997253@users.noreply.github.com>
2018-02-03 19:27:26 -0600

    Fetch multiple commands with -c flag

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,20 @@ fn main() {
     let matches = App::new(APP_NAME)
         .version(VERSION)
         .about("Fast and configurable shell prompter")
-        // .arg(Arg::with_name("exit_code")
-        //          .short("e")
-        //          .long("exit_code")
-        //          .value_name("CODE")
-        //          .help("Exit code of the last-executed command")
-        //          .takes_value(true))
-        .arg(Arg::with_name("config")
-                 .short("c")
-                 .long("config")
-                 .value_name("PATH")
-                 .help("Alternate location for the config file")
-                 .takes_value(true))
-        // .arg(Arg::with_name("shell")
-        //          .long("shell")
-        //          .value_name("SHELL")
-        //          .takes_value(true)
-        //          .possible_values(&["bash", "zsh", "fish", "powershell"]))
+        .arg(
+            Arg::with_name("command")
+                .short("c")
+                .long("command")
+                .value_name("CMD")
+                .help("Command to be executed and inserted into the output")
+                .takes_value(true)
+                .required(true)
+                .multiple(true),
+        )
         .get_matches();
+
+    let commands: Vec<_> = matches.values_of("command").unwrap().collect();
+    for cmd in commands.iter() {
+        println!("{:?}", cmd);
+    }
 }
